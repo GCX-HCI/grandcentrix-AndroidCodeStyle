5a293fbb9db588396d33c42f65182acc8c386bab
StefMa <StefMaDev@outlook.com>
2017-08-18 15:10:44 +0200

    Add automatic 'install' script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Installs GCX AndroidStudio Code Style into your AndroidStudio codestyles folder.
+# Inspired by https://git.io/v7jmC
+
+echo "Installing GCX AndroidStudio configs..."
+echo ""
+
+LATEST_CODE_STYLE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/styles/AndroidTeam_latest.xml"
+for TARGET_DIR in $HOME/Library/Preferences/AndroidStudio*         \
+                  $HOME/Library/Preferences/AndroidStudioPreview*
+
+do
+  if [[ -d $TARGET_DIR ]]; then
+
+    # create codestyles dir and ...
+    mkdir -p ${TARGET_DIR}/codestyles
+    # ... copy to latest style to ${TARGET_DIR}
+    cp -frv ${LATEST_CODE_STYLE} ${TARGET_DIR}/codestyles
+
+  fi
+done
+
+echo ""
+echo "Done."
+echo "Restart AndroidStudio. Go to Preferences->Editor->Code Style and apply Scheme 'AndroidTeam Latest'."
